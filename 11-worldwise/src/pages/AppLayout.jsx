e632997454e19ec29170c94ed296c386151ee39b
Jose <jfernandeztamboleo@gmail.com>
2024-02-12 01:42:47 +0100

    finished 11-worldwise. added lazy loading

--- 11-worldwise/src/pages/AppLayout.jsx
+++ 11-worldwise/src/pages/AppLayout.jsx
@@ -3,7 +3,7 @@ import Map from '../components/Map.jsx'
 import User from '../components/User.jsx'
 import styles from './AppLayout.module.css'
 
-export function AppLayout () {
+export default function AppLayout () {
   return (
     <div className={styles.app}>
       <Sidebar />
