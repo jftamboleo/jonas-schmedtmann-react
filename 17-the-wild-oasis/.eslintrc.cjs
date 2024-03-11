5b927984d65e73567135eb72d275be055e980e5f
Jose <jfernandeztamboleo@gmail.com>
2024-03-11 13:08:26 +0100

    set up react query. create and fetch CabinTable

--- 17-the-wild-oasis/.eslintrc.cjs
+++ 17-the-wild-oasis/.eslintrc.cjs
@@ -29,6 +29,7 @@ module.exports = {
   ],
   rules: {
     'no-unused-vars': 'warn',
-    'react/react-in-jsx-scope': 'off'
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off'
   }
 }
